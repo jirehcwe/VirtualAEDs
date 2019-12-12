4fada0b020e89dc5be8fa9d27b01c04594c13102
Jireh <jireh_cwe@hotmail.com>
2019-12-12 12:14:09 +0800

    Created AED model and cardiac arrest model with animation and variable transitions

---
+++ Assets/Scripts/ARLightEstimator.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class ARLightEstimator : MonoBehaviour
+{
+    public Light m_Light;
+
+    [SerializeField]
+    [Tooltip("The ARCameraManager which will produce frame events containing light estimation information.")]
+    ARCameraManager m_CameraManager;
+
+    /// <summary>
+    /// Get or set the <c>ARCameraManager</c>.
+    /// </summary>
+    public ARCameraManager cameraManager
+    {
+        get { return m_CameraManager; }
+        set
+        {
+            if (m_CameraManager == value)
+                return;
+
+            if (m_CameraManager != null)
+                m_CameraManager.frameReceived -= FrameChanged;
+
+            m_CameraManager = value;
+
+            if (m_CameraManager != null & enabled)
+                m_CameraManager.frameReceived += FrameChanged;
+        }
+    }
+
+    public float? brightness { get; private set; }
+
+    public float? colorTemperature { get; private set; }
+
+    public Color? colorCorrection { get; private set; }
+
+    void Awake ()
+    {
+        if (m_Light == null)
+        {
+            Debug.LogError("No Light object found!");
+        }
+    }
+
+    void OnEnable()
+    {
+        if (m_CameraManager != null)
+            m_CameraManager.frameReceived += FrameChanged;
+    }
+
+    void OnDisable()
+    {
+        if (m_CameraManager != null)
+            m_CameraManager.frameReceived -= FrameChanged;
+    }
+
+    void FrameChanged(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue)
+        {
+            brightness = args.lightEstimation.averageBrightness.Value;
+            m_Light.intensity = brightness.Value;
+        }
+
+        if (args.lightEstimation.averageColorTemperature.HasValue)
+        {
+            colorTemperature = args.lightEstimation.averageColorTemperature.Value;
+            m_Light.colorTemperature = colorTemperature.Value;
+        }
+        
+        if (args.lightEstimation.colorCorrection.HasValue)
+        {
+            colorCorrection = args.lightEstimation.colorCorrection.Value;
+            m_Light.color = colorCorrection.Value;
+        }
+    }
+}
---
+++ Assets/Scripts/ARObjectPlacer.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(ARCursor))]
+[RequireComponent(typeof(LineRenderer))]
+public class ARObjectPlacer : MonoBehaviour
+{
+    enum PlaceMode{
+        Walls,
+        AED,
+        Victim
+    }
+
+    #region Public Fields
+    public GameObject wallPrefab;
+    public float defaultWallHeight = 3;
+
+    public GameObject AEDPrefab;
+
+    public GameObject victimPrefab;
+    #endregion
+
+    #region Private Fields
+    private LineRenderer lineRend;
+    private ARCursor cursor;
+    private Transform wallParent;
+    #endregion
+
+    void Start()
+    {
+        lineRend = GetComponent<LineRenderer>();
+        lineRend.useWorldSpace = true;
+        lineRend.positionCount = 0;
+        lineRend.startWidth = 0.1f;
+        lineRend.endWidth = 0.1f;
+
+        cursor = GetComponent<ARCursor>();
+    }
+
+    void Update()
+    {
+
+    #if UNITY_EDITOR
+        if (Input.GetMouseButtonDown(0))
+        {
+            AddPoint();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            BuildWalls();
+        }
+    #endif
+
+    }
+
+    #region Wall Placement Methods
+    public void AddPoint()
+    {
+        lineRend.positionCount++;
+        lineRend.SetPosition(lineRend.positionCount - 1, cursor.position);
+    }
+
+    public void BuildWalls()
+    {
+        if (wallParent == null)
+        {
+            wallParent = new GameObject("Walls").transform;
+        }
+
+        for (int point = 0; point < lineRend.positionCount-1 ; point++)
+        {
+            PlaceWall(lineRend.GetPosition(point), lineRend.GetPosition(point + 1), wallParent);
+        }
+    }
+
+    void PlaceWall(Vector3 start, Vector3 end, Transform parent = null)
+    {
+        Vector3 midpoint = (start+end)/2;
+        float xDelta = start.x - end.x;
+        float zDelta = start.z - end.z;
+        float wallAngle = Mathf.Atan(zDelta/xDelta)*180/Mathf.PI;
+        if (xDelta < 0)
+        {
+            if (zDelta > 0)
+            {
+                wallAngle = -wallAngle + 180;
+            }
+            else
+            {
+                wallAngle = 180 - wallAngle;
+            }
+        }
+        else
+        {
+                wallAngle = -wallAngle;
+        }
+        Quaternion wallRotation = Quaternion.Euler(0, wallAngle, 0);
+        Transform wallT = Instantiate(wallPrefab, midpoint, wallRotation, parent).transform;
+        float length = (start-end).magnitude;
+        wallT.localScale = new Vector3(length, defaultWallHeight, length);
+    }
+    #endregion
+    
+}
