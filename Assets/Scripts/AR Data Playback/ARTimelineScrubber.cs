ae5b1f3d107d311f13213a91a417b949d15f87de
Jireh <jireh_cwe@hotmail.com>
2020-03-08 20:26:34 +0800

    temp changes for reinstalling vscode

---
+++ Assets/Scripts/AR Data Playback/ARTimelineScrubber.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ARTimelineScrubber : MonoBehaviour
+{
+
+    #region Public Fields
+    #endregion
+
+    #region Private Fields
+    private 
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
