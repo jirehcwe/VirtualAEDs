d231abc2eda0c4ea75685b3c5c34d59fbbfb2e59
Jireh <jireh_cwe@hotmail.com>
2020-09-04 15:24:50 +0800

    Add victim cardiac arrest timer UI (no logic yet)

---
+++ Assets/Scripts/AR World Creation/UpdateTextFieldFromSlider.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdateTextFieldFromSlider : MonoBehaviour
+{
+
+    #region Public Fields
+    public Slider slider;
+    public Text text;
+    #endregion
+
+    public void UpdateTextfield()
+    {
+        text.text = slider.value + " seconds";
+    }
+}
